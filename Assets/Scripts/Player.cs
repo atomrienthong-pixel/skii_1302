8accc14d1bbc7508cd388a4533011aee420d9838
atomrienthong-pixel <atomrienthong@gmail.com>
2026-08-01 15:33:01 +0700

    hp--

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float forcePower;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    private InputAction moveActopn;
+    private Vector2 moveValue;
+
+    [SerializeField]
+    private int point;
+    public int Point { get { return point; } set { point = value; } }
+
+    [SerializeField]
+    private int hp;
+    public int HP { get { return hp; } set { hp = value; } }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveActopn = InputSystem.actions.FindAction("Move");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveLeftOrRight();
+    }
+
+    private void MoveLeftOrRight()
+    {
+        moveValue = moveActopn.ReadValue<Vector2>();
+        rb.AddForce(moveValue.x * Vector3.right * forcePower);
+    }
+}
---
+++ Assets/Scripts/TreeObstacle.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class TreeObstacle : MonoBehaviour
+{
+    [SerializeField]
+    private Color hitColor = Color.red;
+
+    [SerializeField]
+    private int damage = 1;
+
+    [SerializeField]
+    private Renderer treeRenderer;
+
+    private Color originalColor;
+
+    void Start()
+    {
+        if (treeRenderer == null)
+            treeRenderer = GetComponentInChildren<Renderer>();
+
+        originalColor = treeRenderer.material.color;
+    }
+
+    // ชนแบบ collider ปกติ (Is Trigger ปิด)
+    private void OnCollisionEnter(Collision collision)
+    {
+        HandleEnter(collision.gameObject);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        HandleExit(collision.gameObject);
+    }
+
+    // เผื่อกรณีเปลี่ยน collider เป็น Is Trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        HandleEnter(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        HandleExit(other.gameObject);
+    }
+
+    private void HandleEnter(GameObject other)
+    {
+        Player player = other.GetComponentInParent<Player>();
+        if (player == null)
+            return;
+
+        treeRenderer.material.color = hitColor;
+        player.HP -= damage;
+    }
+
+    private void HandleExit(GameObject other)
+    {
+        if (other.GetComponentInParent<Player>() == null)
+            return;
+
+        treeRenderer.material.color = originalColor;
+    }
+}
