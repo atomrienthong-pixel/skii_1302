08e7cd010851b3a8fae6554e14a9a20325dad9e8
atomrienthong-pixel <atomrienthong@gmail.com>
2026-08-24 09:22:36 +0700

    แฟ

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float forcePower = 20f;
+    private float forcePower = 900f;
 
     [SerializeField]
     private int point;
